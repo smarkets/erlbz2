2f9c30b6cfef7bd610e93d18defa0f21f7019660
Knut Nesheim <knutin@gmail.com>
2013-08-01 16:30:03 +0200

    Use proper to generate data failing compression.

--- test/prop_com_decom.erl
+++ test/prop_com_decom.erl
@@ -4,5 +4,31 @@
 
 -include_lib("proper/include/proper.hrl").
 
+
+bin() ->
+    ?SIZED(S, bin(S)).
+
+bin(N) ->
+    Chars = "ABCDEFGIJKLMNOPQRSTUVZ"
+        "abcdefgijklmnopqrstuvz"
+        "0123456789"
+        "{}"
+        "\",.",
+    iolist_to_binary(
+      [lists:nth(random:uniform(length(Chars)), Chars) || _ <- lists:seq(1, N)]).
+
+
+
 prop_compress_decompress() ->
-    ?FORALL(Msg, binary(), Msg =:= erlbz2:decompress(erlbz2:compress(Msg))).
+    ?FORALL(Msg, bin(),
+            begin
+                try
+                    Msg =:= erlbz2:decompress(erlbz2:compress(Msg))
+                catch
+                    error:sequence_error ->
+                        error_logger:info_msg("size: ~p~n~p~n",
+                                              [byte_size(Msg), Msg]),
+                        false
+                end
+            end).
+
